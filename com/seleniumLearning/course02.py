056523c56c8ce177d2e68bd20e75a299285544ea
Tao Lee <281617953@qq.com>
2019-07-18 15:54:38 +0800

    update

--- com/seleniumLearning/course02.py
+++ com/seleniumLearning/course02.py
@@ -6,9 +6,22 @@ brower.get("http://www.baidu.com")
 print(brower.title)
 if "百度" in brower.title:
     print(brower.current_url)
-    z  = brower.find_element_by_id("kw")
-    print(type(z))  #webelement
+    z = brower.find_element_by_id("kw")
+    print(type(z))  # webelement
     z.clear()
     z.send_keys("李涛涛")
     baidu = brower.find_element_by_id("su")
     baidu.submit()
+
+
+def test_send_keys(url, id_name, context):
+    b = webdriver.Firefox()
+    b.get(url)
+
+    ele = b.find_element_by_id(id_name)
+    if ele is None:
+        return 0
+    ele.send_keys(context)
+    ele2 = b.find_element_by_id("su")  # 提交按钮
+    ele2.submit()
+    return 0
--- com/seleniumLearning/course03.py
+++ com/seleniumLearning/course03.py
@@ -7,7 +7,8 @@ z = b.find_element_by_link_text("Python全栈开发")
 z.click()
 b.back()
 
-ele = b.find_element_by_css_selector('html body.YaHei.index div.main div.side-bar div.side-menu.w1920 div.w1920-scroll div.side-nav ul li.icon-3 a')
+ele = b.find_element_by_css_selector(
+    'html body.YaHei.index div.main div.side-bar div.side-menu.w1920 div.w1920-scroll div.side-nav ul li.icon-3 a')
 ele.click()
 b.back()
 
@@ -16,5 +17,14 @@ ele.click()
 b.back()
 
 
-
-
+def test_find_xpath(url, xpath):
+    #open the webdrive
+    b = webdriver.Firefox()
+    b.get(url)
+    eles = b.find_elements_by_xpath(xpath)
+    if len(eles) > 1:
+        eles[0].click()
+        b.back()
+    #close the driver
+    b.close()
+    return eles
